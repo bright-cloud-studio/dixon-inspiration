41e628c666e13ea1c7e75520e7af7b5d39d7f728
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-04-08 14:07:52 -0400

    Create BcsInspirationBundle.php

---
+++ src/BcsInspirationBundle.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @copyright  Bright Cliud Studio
+ * @author     Bright Cloud Studio
+ * @package    Contao Dixon Inspiration
+ * @license    LGPL-3.0+
+ * @see	       https://github.com/bright-cloud-studio/dixon-inspiration
+ */
+
+namespace Bcs\InspirationBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class BcsInspirationBundle extends Bundle
+{
+}
