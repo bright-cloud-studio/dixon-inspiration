577cb7621f554bce98fdaab4ddc30ce87082d4f7
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-04-08 14:49:38 -0400

    Update tl_news.php

--- src/Resources/contao/dca/tl_news.php
+++ src/Resources/contao/dca/tl_news.php
@@ -8,19 +8,4 @@
     * @see	       https://github.com/bright-cloud-studio/dixon-inspiration
     */
     
-    
-    $dc = &$GLOBALS['TL_DCA']['tl_news'];
-    
-    /* Extend the tl_news palettes */
-    $GLOBALS['TL_DCA']['tl_news']['palettes']['default'] = str_replace(';{teaser_legend}', ';{inspiration_legend},test_field;{teaser_legend}', $GLOBALS['TL_DCA']['tl_news']['palettes']['default']);
-    
-    $arrFields = array(
-        'inspiration_1'             => array(
-        'label'                    => &$GLOBALS['TL_LANG']['tl_news']['inspiration_1'],
-        'inputType'                => 'text',
-        'eval'                     => array('tl_class'=>'w50'),
-        'sql'                      => "mediumtext NULL"
-    )
-);
 
-$dc['fields'] = array_merge($dc['fields'], $arrFields);
