d41ac7e4b2f8677bec4f1606c90d04da0a034a25
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-04-08 14:45:29 -0400

    Update tl_news.php

--- src/Resources/contao/dca/tl_news.php
+++ src/Resources/contao/dca/tl_news.php
@@ -1,22 +1,26 @@
 <?php
 
-  /**
-  * @copyright  Bright Cliud Studio
-  * @author     Bright Cloud Studio
-  * @package    Contao Dixon Inspiration
-  * @license    LGPL-3.0+
-  * @see	       https://github.com/bright-cloud-studio/dixon-inspiration
-  */
-  
-  /* Extend the tl_news palettes */
-  $GLOBALS['TL_DCA']['tl_news']['palettes']['default'] = str_replace(';{teaser_legend}', ';{inspiration_legend},test_field;{teaser_legend}', $GLOBALS['TL_DCA']['tl_news']['palettes']['default']);
-  
-  $GLOBALS['TL_DCA']['tl_news']['fields']['test_field'] = array
-  (
-    'label'                   => &$GLOBALS['TL_LANG']['tl_news']['test_field'],
-    'inputType'               => 'text',
-    'default'                 => '',
-    'search'                  => true,
-    'eval'                    => array('mandatory'=>false, 'tl_class'=>'w50'),
-    'sql'                     => "varchar(255) NOT NULL default ''"
-  );
+    /**
+    * @copyright  Bright Cliud Studio
+    * @author     Bright Cloud Studio
+    * @package    Contao Dixon Inspiration
+    * @license    LGPL-3.0+
+    * @see	       https://github.com/bright-cloud-studio/dixon-inspiration
+    */
+    
+    
+    $dc = &$GLOBALS['TL_DCA']['tl_news'];
+    
+    /* Extend the tl_news palettes */
+    $GLOBALS['TL_DCA']['tl_news']['palettes']['default'] = str_replace(';{teaser_legend}', ';{inspiration_legend},test_field;{teaser_legend}', $GLOBALS['TL_DCA']['tl_news']['palettes']['default']);
+    
+    $arrFields = array(
+        'inspiration_1'             => array(
+        'label'                    => &$GLOBALS['TL_LANG']['tl_news']['inspiration_1'],
+        'inputType'                => 'text',
+        'eval'                     => array('tl_class'=>'w50'),
+        'sql'                      => "mediumtext NULL"
+    )
+);
+
+$dc['fields'] = array_merge($dc['fields'], $arrFields);
