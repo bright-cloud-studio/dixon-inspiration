c49e13467814aa6135f44adbf7133f5d82a32db4
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-04-08 15:00:46 -0400

    Update tl_news.php

--- src/Resources/contao/dca/tl_news.php
+++ src/Resources/contao/dca/tl_news.php
@@ -7,17 +7,3 @@
  * @license    LGPL-3.0+
  * @see	       https://github.com/bright-cloud-studio/contao-ce-chart
  */
-
-// Get our default 'tl_news' DCA
-$dc = &$GLOBALS['TL_DCA']['tl_news'];
-
-$arrFields = array(
-    'bar_border_width'        => array(
-        'label'                    => &$GLOBALS['TL_LANG']['tl_news']['bar_border_width'],
-        'inputType'                => 'text',
-		'eval'                     => array('tl_class'=>'w50'),
-		'sql'                      => "varchar(12) NOT NULL default '0'"
-    ),
-);
-
-$dc['fields'] = array_merge($dc['fields'], $arrFields);
