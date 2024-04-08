1976fb29d6bd77e2538837af9d74c9c10fc654c7
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-04-08 14:11:47 -0400

    Create config.php

---
+++ src/Resources/contao/config/config.php
@@ -0,0 +1,23 @@
+<?php
+
+  /**
+  * @copyright  Bright Cliud Studio
+  * @author     Bright Cloud Studio
+  * @package    Contao Dixon Inspiration
+  * @license    LGPL-3.0+
+  * @see	       https://github.com/bright-cloud-studio/dixon-inspiration
+  */
+  
+  //array_insert($GLOBALS['TL_CTE']['glide'], -1, array
+  //(
+  //  'glide_start' => 'Bcs\GlideBundle\ContentGlideStart',
+  //  'glide_stop' => 'Bcs\GlideBundle\ContentGlideStop',
+  //  'glide_gallery' => 'Bcs\GlideBundle\ContentGlideGallery'
+  //));
+  
+  
+  // Declare both of our new Content Elements as wrappers so we get the cool indenting in Content > Articles
+  //$GLOBALS['TL_WRAPPERS']['start'][] = 'glide_start';
+  //$GLOBALS['TL_WRAPPERS']['stop'][] = 'glide_stop';
+
+?>
