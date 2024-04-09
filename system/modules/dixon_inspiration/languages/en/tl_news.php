552e4d52a95aabe77ae3e8c08df33e0551197e95
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-04-09 14:58:05 -0400

    Update tl_news.php

--- system/modules/dixon_inspiration/languages/en/tl_news.php
+++ system/modules/dixon_inspiration/languages/en/tl_news.php
@@ -6,7 +6,7 @@ $GLOBALS['TL_LANG']['tl_news']['inspiration_legend'] = 'Inspiration Settings';
 /* Fields */
 $GLOBALS['TL_LANG']['tl_news']['inspiration_steps']   = array('Step Details', 'Fill in the headline, text, image and video to be used for this Step on the frontend.');
 
-$GLOBALS['TL_LANG']['tl_news']['insp_headline']       = array('Step Headline', 'Enter details of this Step');
+$GLOBALS['TL_LANG']['tl_news']['inspiration_steps']['insp_headline']       = array('Step Headline', 'Enter details of this Step');
 $GLOBALS['TL_LANG']['tl_news']['insp_text']           = array('Step Text', 'The headline that displays at the top of this Step');
 $GLOBALS['TL_LANG']['tl_news']['insp_image']          = array('Step Image', 'The image that displays as part of this Step');
 $GLOBALS['TL_LANG']['tl_news']['insp_video']          = array('Step Video', 'The video that displays as part of this Step');
