392a56197708dfa3d6868f7d6afa6b5823073e1a
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-04-09 13:14:54 -0400

    Create tl_news.php

---
+++ system/modules/dixon_inspiration/dca/tl_news.php
@@ -0,0 +1,15 @@
+<?php
+
+/* Extend the tl_news palettes */
+$GLOBALS['TL_DCA']['tl_news']['palettes']['default'] = str_replace(';{teaser_legend}', ';{add_news_fields_legend},news_issue;{teaser_legend}', $GLOBALS['TL_DCA']['tl_news']['palettes']['default']);
+
+/* Add new field */
+$GLOBALS['TL_DCA']['tl_news']['fields']['testy'] = array
+(
+  'label'                   => &$GLOBALS['TL_LANG']['tl_news']['testy'],
+  'inputType'               => 'text',
+  'default'                 => '',
+  'search'                  => true,
+  'eval'                    => array('mandatory'=>false, 'tl_class'=>'w50'),
+  'sql'                     => "varchar(255) NOT NULL default ''"
+);
