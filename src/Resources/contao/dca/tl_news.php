cb1c37a53644bd40ecf0cd8413dbc709911c743e
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-04-09 10:47:40 -0400

    Update tl_news.php

--- src/Resources/contao/dca/tl_news.php
+++ src/Resources/contao/dca/tl_news.php
@@ -7,3 +7,17 @@
  * @license    LGPL-3.0+
  * @see	       https://github.com/bright-cloud-studio/contao-ce-chart
  */
+
+
+
+
+// Fields
+$GLOBALS['TL_DCA']['tl_news']['fields']['test_1'] = array
+(
+    'label'            => &$GLOBALS['TL_LANG']['MSC']['test_1'],
+    'exclude'          => true,
+    'inputType'        => 'select',
+    'search'           => true,
+    'eval'             => array('chosen' => true, 'multiple' => true, 'tl_class' => 'clr'),
+    'sql'              => "blob NULL"
+);
