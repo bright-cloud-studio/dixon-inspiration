2917e80bce077a6b7e7e696ef2c7e46a139aca94
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-04-09 12:53:28 -0400

    Update tl_news.php

--- src/Resources/contao/dca/tl_news.php
+++ src/Resources/contao/dca/tl_news.php
@@ -1,11 +1,11 @@
 <?php
 
-$GLOBALS['TL_DCA']['tl_news']['fields'] += [
-    'testy2' => [
-        'label' => &$GLOBALS['TL_LANG']['tl_content']['testy2'],
-        'exclude' => true,
-        'inputType' => 'text',
-        'eval' => ['tl_class' => 'w50', 'required' => true, 'rgxp' => 'recaptcha'],
-        'sql' => "varchar(120) NOT NULL default ''",
-    ],
-];
+$GLOBALS['TL_DCA']['tl_news']['fields']['news_issue'] = array
+(
+	'label'                   => &$GLOBALS['TL_LANG']['tl_news']['news_issue'],
+	'inputType'               => 'text',
+	'default'                 => '',
+	'search'                  => true,
+	'eval'                    => array('mandatory'=>false, 'tl_class'=>'w50'),
+	'sql'                     => "varchar(255) NOT NULL default ''"
+);
