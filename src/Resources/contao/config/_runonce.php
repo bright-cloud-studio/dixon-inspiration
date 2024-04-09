44421ae07789dc5b454f586bccda6efc95a59fcd
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-04-09 12:34:16 -0400

    Update _runonce.php

--- src/Resources/contao/config/_runonce.php
+++ src/Resources/contao/config/_runonce.php
@@ -4,21 +4,16 @@
   {
       public function __construct()
       {
-          //parent::__construct();
-          //$this->import('Files');
+          parent::__construct();
+          $this->import('Files');
       }
       public function run()
       {
-          /*
-          if ((@include(TL_ROOT . '/system/modules/Glide/config/config.php')) !== false)
-          {
-              $this->Files->rrdir('system/modules/Glide');
-          }
-          */
+          
       }
   }
   
-  //$objGlideRunonceJob = new GlideRunonceJobTemp();
-  //$objGlideRunonceJob->run();
+  $objInspirationRunonceJob = new GlideRunonceJobTemp();
+  $objInspirationRunonceJob->run();
 
 ?>
