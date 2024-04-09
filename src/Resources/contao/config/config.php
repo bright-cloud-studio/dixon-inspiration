b086ef17eb64ec4e20208312cc9d404806adef93
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-04-09 11:49:56 -0400

    Update config.php

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -1,23 +1,18 @@
 <?php
+ 
+/**
+* @copyright  Bright Cliud Studio
+* @author     Bright Cloud Studio
+* @package    Contao CE Recaptcha
+* @license    LGPL-3.0+
+* @see	       https://github.com/bright-cloud-studio/contao-ce-recaptcha
+*/
 
-  /**
-  * @copyright  Bright Cliud Studio
-  * @author     Bright Cloud Studio
-  * @package    Contao Dixon Inspiration
-  * @license    LGPL-3.0+
-  * @see	       https://github.com/bright-cloud-studio/dixon-inspiration
-  */
-  
-  //array_insert($GLOBALS['TL_CTE']['glide'], -1, array
-  //(
-  //  'glide_start' => 'Bcs\GlideBundle\ContentGlideStart',
-  //  'glide_stop' => 'Bcs\GlideBundle\ContentGlideStop',
-  //  'glide_gallery' => 'Bcs\GlideBundle\ContentGlideGallery'
-  //));
-  
-  
-  // Declare both of our new Content Elements as wrappers so we get the cool indenting in Content > Articles
-  //$GLOBALS['TL_WRAPPERS']['start'][] = 'glide_start';
-  //$GLOBALS['TL_WRAPPERS']['stop'][] = 'glide_stop';
 
-?>
+// Setup our Form field
+//array_insert($GLOBALS['TL_FFL']['recaptcha'], -1, FormRecaptcha::class);
+
+// Setup our background content element
+//array_insert($GLOBALS['TL_CTE']['miscellaneous'], 0, [
+ //   'backgroundrecaptcha' => Bcs\ElementRecaptcha::class,
+//]);
