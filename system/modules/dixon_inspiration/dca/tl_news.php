1259ee7bdd81c256b6ab56c87be58ce267e37437
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-04-09 15:01:11 -0400

    Update tl_news.php

--- system/modules/dixon_inspiration/dca/tl_news.php
+++ system/modules/dixon_inspiration/dca/tl_news.php
@@ -12,25 +12,25 @@ $GLOBALS['TL_DCA']['tl_news']['fields']['inspiration_steps'] = array
         'eval'      => [
             'columnFields' => [
                 'insp_headline' => [
-                    'label'     => &$GLOBALS['TL_LANG']['tl_content']['insp_headline'],
+                    'label'     => &$GLOBALS['TL_LANG']['tl_news']['insp_headline'],
                     'exclude'   => true,
                     'inputType' => 'text',
                     'eval'      => [ 'style' => 'width:100%', 'columnPos' => 'group_1' ],
                 ],
                 'insp_text' => [
-                    'label'     => &$GLOBALS['TL_LANG']['tl_content']['insp_text'],
+                    'label'     => &$GLOBALS['TL_LANG']['tl_news']['insp_text'],
                     'exclude'   => true,
                     'inputType' => 'textarea',
                     'eval'      => array('tl_class'=>'clr', 'rte'=>'tinyMCE', 'style' => 'width:100%', 'columnPos' => 'group_1'),
                 ],
                 'insp_image' => [
-                    'label'     => &$GLOBALS['TL_LANG']['tl_content']['insp_image'],
+                    'label'     => &$GLOBALS['TL_LANG']['tl_news']['insp_image'],
                     'exclude'   => true,
                     'inputType' => 'text',
                     'eval'      => [ 'style' => 'width:100%', 'columnPos' => 'group_1' ],
                 ],
                 'insp_video' => [
-                    'label'     => &$GLOBALS['TL_LANG']['tl_content']['insp_video'],
+                    'label'     => &$GLOBALS['TL_LANG']['tl_news']['insp_video'],
                     'exclude'   => true,
                     'inputType' => 'text',
                     'eval'      => [ 'style' => 'width:100%', 'columnPos' => 'group_1' ],
