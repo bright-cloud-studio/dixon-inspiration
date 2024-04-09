963a30190c62a66cf044de28a24028e9dfd224c6
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-04-09 13:15:57 -0400

    Create tl_news.php

---
+++ system/modules/dixon_inspiration/languages/en/tl_news.php
@@ -0,0 +1,7 @@
+<?php
+
+/* Legends */
+$GLOBALS['TL_LANG']['tl_news']['inspiration_legend'] = 'Inspiration Settings';
+
+/* Fields */
+$GLOBALS['TL_LANG']['tl_news']['testy']          = array('Testy1234', 'Testing a new custom field');
