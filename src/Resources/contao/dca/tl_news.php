1b479334f3b3aa7a3060646476db86867a10accf
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-04-09 12:55:31 -0400

    Update tl_news.php

--- src/Resources/contao/dca/tl_news.php
+++ src/Resources/contao/dca/tl_news.php
@@ -1,11 +1,15 @@
 <?php
 
+// Extend the 'tl_news' Palette
+//$GLOBALS['TL_DCA']['tl_news']['palettes']['default'] = str_replace(';{teaser_legend}', ';{add_news_fields_legend},news_issue;{teaser_legend}', $GLOBALS['TL_DCA']['tl_news']['palettes']['default']);
+
+// Add new 'tl_news' fields
 $GLOBALS['TL_DCA']['tl_news']['fields']['news_issue'] = array
 (
-	'label'                   => &$GLOBALS['TL_LANG']['tl_news']['news_issue'],
-	'inputType'               => 'text',
-	'default'                 => '',
-	'search'                  => true,
-	'eval'                    => array('mandatory'=>false, 'tl_class'=>'w50'),
-	'sql'                     => "varchar(255) NOT NULL default ''"
+    'label'                   => &$GLOBALS['TL_LANG']['tl_news']['news_issue'],
+    'inputType'               => 'text',
+    'default'                 => '',
+    'search'                  => true,
+    'eval'                    => array('mandatory'=>false, 'tl_class'=>'w50'),
+    'sql'                     => "varchar(255) NOT NULL default ''"
 );
