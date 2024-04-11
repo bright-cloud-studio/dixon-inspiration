94ab467aaed5e21a7a458b0e8491bc576982250d
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-04-11 15:04:36 -0400

    Update tl_news.php

--- system/modules/dixon_inspiration/dca/tl_news.php
+++ system/modules/dixon_inspiration/dca/tl_news.php
@@ -1,6 +1,7 @@
 <?php
 
 /* Extend the tl_news palettes */
+$GLOBALS['TL_DCA']['tl_news']['palettes']['default'] = str_replace(';{title_legend}', ';{post_type_legend},post_type;{title_legend}', $GLOBALS['TL_DCA']['tl_news']['palettes']['default']);
 $GLOBALS['TL_DCA']['tl_news']['palettes']['default'] = str_replace(';{teaser_legend}', ';{inspiration_legend},inspiration_steps;{teaser_legend}', $GLOBALS['TL_DCA']['tl_news']['palettes']['default']);
 
 /* Add new field */
@@ -39,4 +40,14 @@ $GLOBALS['TL_DCA']['tl_news']['fields']['inspiration_steps'] = array
         'tl_class' => 'clr',
     ],
     'sql'       => 'blob NULL',
+);
+
+/* Adds a select at the top of a News content article to allow selecting which type of news post this is, news or inspiration */
+$GLOBALS['TL_DCA']['tl_news']['fields']['post_type'] = array
+(
+    'label'        => &$GLOBALS['TL_LANG']['tl_news']['post_type'],
+    'inputType'    => 'select',
+    'options'      => array('news' => 'News', 'inspiration' => 'Inspiration'),
+    'eval'         => array('mandatory'=>true, 'tl_class'=>'w50'),
+    'sql'          => "varchar(32) NOT NULL default 'yes'"
 );
