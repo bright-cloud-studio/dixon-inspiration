24c141f6f477cb3fa0d588b1c54f0dbe886b7a9a
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-04-22 13:28:55 -0400

    Create autoload.php

---
+++ system/modules/dixon_inspiration/config/autoload.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Bright Cloud Studio's Dixon Inspiration
+ *
+ * Copyright (C) 2024 Bright Cloud Studio
+ *
+ * @package    bright-cloud-studio/dixon-inspiration
+ * @link       https://www.brightcloudstudio.com/
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+**/
+
+
+// Register Classes
+ClassLoader::addClasses(array
+(
+    'Bcs\Module\ModuleInspirationNewsList'         => 'system/modules/dixon_inspiration/library/Bcs/Module/ModuleInspirationNewsList.php'
+));
