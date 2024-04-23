d54914486753a120934337f46524f42b674f609e
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-04-23 14:45:48 -0400

    Update tl_module.php

--- system/modules/dixon_inspiration/dca/tl_module.php
+++ system/modules/dixon_inspiration/dca/tl_module.php
@@ -1,4 +1,4 @@
 <?php
 
 // Palette for our module
-$GLOBALS['TL_DCA']['tl_module']['palettes']['mod_inspiration_news'] = '{title_legend},headline,featured,alias,author;{date_legend},date,time;{source_legend:hide},source;{meta_legend},pageTitle,robots,description,serpPreview;{teaser_legend},subheadline,teaser;{image_legend},addImage;{enclosure_legend:hide},addEnclosure;{expert_legend:hide},cssClass,noComments;{publish_legend},published,start,stop';
+$GLOBALS['TL_DCA']['tl_module']['palettes']['mod_inspiration_news']['default'] = '{title_legend},headline,featured,alias,author;{date_legend},date,time;{source_legend:hide},source;{meta_legend},pageTitle,robots,description,serpPreview;{teaser_legend},subheadline,teaser;{image_legend},addImage;{enclosure_legend:hide},addEnclosure;{expert_legend:hide},cssClass,noComments;{publish_legend},published,start,stop';
