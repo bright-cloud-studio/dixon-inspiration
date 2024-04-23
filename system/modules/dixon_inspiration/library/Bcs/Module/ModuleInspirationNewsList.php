f36b25c66dac897ffd500061ba900f98e08d98d6
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-04-23 14:28:55 -0400

    Update ModuleInspirationNewsList.php

--- system/modules/dixon_inspiration/library/Bcs/Module/ModuleInspirationNewsList.php
+++ system/modules/dixon_inspiration/library/Bcs/Module/ModuleInspirationNewsList.php
@@ -53,7 +53,86 @@ class ModuleInspirationNewsList extends ModuleInspirationNews
 	 */
 	protected function __compile()
 	{
-		parent::compile();
+		$limit = null;
+		$offset = $this->skipFirst;
+
+		// Maximum number of items
+		if ($this->numberOfItems > 0)
+		{
+			$limit = $this->numberOfItems;
+		}
+
+		// Handle featured news
+		if ($this->news_featured == 'featured')
+		{
+			$blnFeatured = true;
+		}
+		elseif ($this->news_featured == 'unfeatured')
+		{
+			$blnFeatured = false;
+		}
+		else
+		{
+			$blnFeatured = null;
+		}
+
+		$this->Template->articles = array();
+		$this->Template->empty = $GLOBALS['TL_LANG']['MSC']['emptyList'];
+
+		// Get the total number of items
+		$intTotal = $this->countItems($this->news_archives, $blnFeatured);
+
+		if ($intTotal < 1)
+		{
+			return;
+		}
+
+		$total = $intTotal - $offset;
+
+		// Split the results
+		if ($this->perPage > 0 && (!isset($limit) || $this->numberOfItems > $this->perPage))
+		{
+			// Adjust the overall limit
+			if (isset($limit))
+			{
+				$total = min($limit, $total);
+			}
+
+			// Get the current page
+			$id = 'page_n' . $this->id;
+			$page = (int) (Input::get($id) ?? 1);
+
+			// Do not index or cache the page if the page number is outside the range
+			if ($page < 1 || $page > max(ceil($total/$this->perPage), 1))
+			{
+				throw new PageNotFoundException('Page not found: ' . Environment::get('uri'));
+			}
+
+			// Set limit and offset
+			$limit = $this->perPage;
+			$offset += (max($page, 1) - 1) * $this->perPage;
+			$skip = $this->skipFirst;
+
+			// Overall limit
+			if ($offset + $limit > $total + $skip)
+			{
+				$limit = $total + $skip - $offset;
+			}
+
+			// Add the pagination menu
+			$objPagination = new Pagination($total, $this->perPage, Config::get('maxPaginationLinks'), $id);
+			$this->Template->pagination = $objPagination->generate("\n  ");
+		}
+
+		$objArticles = $this->fetchItems($this->news_archives, $blnFeatured, $limit ?: 0, $offset);
+
+		// Add the articles
+		if ($objArticles !== null)
+		{
+			$this->Template->articles = $this->parseArticles($objArticles);
+		}
+
+		$this->Template->archives = $this->news_archives;
 	}
 
 }
