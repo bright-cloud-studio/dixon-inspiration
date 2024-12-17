98f5a1de099a3ad1e1bf46ea5195b308393a19cb
Bright Cloud Studio <mark@brightcloudstudio.com>
2024-12-17 10:46:39 -0500

    Create config.php

---
+++ src/Resources/contao/config/config.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+* @copyright  Bright Cliud Studio
+* @author     Bright Cloud Studio
+* @package    Dixon Inspiration
+* @license    LGPL-3.0+
+* @see	       https://github.com/bright-cloud-studio/dixon-inspiration
+*/
+
+/* Front End modules */
+//$GLOBALS['FE_MOD']['gai']['mod_receive_data'] = 'Bcs\Module\ModReceiveData';
