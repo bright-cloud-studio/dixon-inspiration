674905e61bbd3315deb14199eccdcc86a56918df
Bright Cloud Studio <mark@brightcloudstudio.com>
2024-12-17 12:31:13 -0500

    Update tl_news.php

--- src/Resources/contao/dca/tl_news.php
+++ src/Resources/contao/dca/tl_news.php
@@ -1,19 +1,19 @@
 <?php
 
  /* Extend the tl_news palettes */
-$GLOBALS['TL_DCA']['tl_news']['palettes']['default'] = str_replace(';{enclosure_legend:hide}', ';{type_legend},news_type;{enclosure_legend:hide}', $GLOBALS['TL_DCA']['tl_news']['palettes']['default']);
+$GLOBALS['TL_DCA']['tl_news']['palettes']['default'] = str_replace(';{enclosure_legend:hide}', ';{type_legend},newsType;{enclosure_legend:hide}', $GLOBALS['TL_DCA']['tl_news']['palettes']['default']);
 
-$GLOBALS['TL_DCA']['tl_news']['palettes']['__selector__'] = array('source', 'addImage', 'addEnclosure', 'overwriteMeta', 'news_type');
-$GLOBALS['TL_DCA']['tl_news']['subpalettes']['step_by_step'] = 'singleSRCMainImage';
-$GLOBALS['TL_DCA']['tl_news']['subpalettes']['activity'] = 'sizeMainImage';
+$GLOBALS['TL_DCA']['tl_news']['palettes']['__selector__'] = array('source', 'addImage', 'addEnclosure', 'overwriteMeta', 'newsType');
+$GLOBALS['TL_DCA']['tl_news']['subpalettes']['newsType_step'] = 'singleSRCMainImage';
+$GLOBALS['TL_DCA']['tl_news']['subpalettes']['newsType_activity'] = 'sizeMainImage';
 
 
 
-$GLOBALS['TL_DCA']['tl_news']['fields']['news_type'] = array
+$GLOBALS['TL_DCA']['tl_news']['fields']['newsType'] = array
 (
-    'label'                    => &$GLOBALS['TL_LANG']['tl_news']['news_type'],
+    'label'                    => &$GLOBALS['TL_LANG']['tl_news']['newsType'],
     'inputType'                => 'select',
-    'options'                  => array('step_by_step' => 'Step-by-Step', 'activity' => 'Activity', 'article' => 'Article'),
+    'options'                  => array('step' => 'Step-by-Step', 'activity' => 'Activity', 'article' => 'Article'),
     'eval'                     => array('submitOnChange'=>true, 'mandatory'=>true, 'tl_class'=>'w50'),
     'default'                   => '',
     'sql'                       => "text default ''"
