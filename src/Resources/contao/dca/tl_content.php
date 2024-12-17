3693b32405e8d007ed7908626889306918129422
Bright Cloud Studio <mark@brightcloudstudio.com>
2024-12-17 14:36:59 -0500

    Update tl_content.php

--- src/Resources/contao/dca/tl_content.php
+++ src/Resources/contao/dca/tl_content.php
@@ -10,29 +10,29 @@
 
 // Get our default 'tl_content' DCA
 $dc = &$GLOBALS['TL_DCA']['tl_content'];
-$GLOBALS['TL_DCA']['tl_content']['palettes']['chart_line'] = '{type_legend},type,headline;{dataset_legend},tableitems,label_x,label_y,chart_desc;{chart_line_config_legend},animate,line_tension,line_border_width,line_border_dash,line_border_joint_style,line_fill;{line_border_colors_legend},line_border_colors;{line_background_colors_legend},line_background_colors;{line_point_legend},line_point_background_color,line_point_border_color,line_point_border_width,line_point_style,line_point_radius;{chart_options_legend},maintain_aspect_ratio,responsive,max_width,max_height;{template_legend:hide},customTpl;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID;{invisible_legend:hide},invisible,start,stop';
-$GLOBALS['TL_DCA']['tl_content']['palettes']['chart_bar'] = '{type_legend},type,headline;{dataset_legend},tableitems,label_x,label_y,chart_desc;{chart_bar_config_legend},bar_border_width,bar_border_skipped,bar_border_radius,bar_inflate_amount,bar_point_style;{bar_background_colors_legend},bar_background_colors;{bar_border_colors_legend},bar_border_colors;{chart_options_legend},maintain_aspect_ratio,responsive,max_width,max_height;{template_legend:hide},customTpl;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID;{invisible_legend:hide},invisible,start,stop';
+$GLOBALS['TL_DCA']['tl_content']['palettes']['activity'] = '{type_legend},type,headline;{activity_legend},activity_text;{template_legend:hide},customTpl;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID;{invisible_legend:hide},invisible,start,stop';
+$GLOBALS['TL_DCA']['tl_content']['palettes']['step'] = '{type_legend},type,headline;{step_legend},step_text, step_picture;{template_legend:hide},customTpl;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID;{invisible_legend:hide},invisible,start,stop';
 
 $arrFields = array(
-    'label_x'             => array(
-        'label'                    => &$GLOBALS['TL_LANG']['tl_content']['label_x'],
+    'activity_text'             => array(
+        'label'                    => &$GLOBALS['TL_LANG']['tl_content']['activity_text'],
         'inputType'                => 'text',
 		'eval'                     => array('tl_class'=>'w50'),
 		'sql'                      => "mediumtext NULL"
     ),
-    'line_background_colors'       => array(
-        'label'     => &$GLOBALS['TL_LANG']['tl_content']['line_background_colors'],
+    'step'       => array(
+        'label'     => &$GLOBALS['TL_LANG']['tl_content']['activity'],
         'exclude'   => true,
         'inputType' => 'multiColumnWizard',
         'eval'      => [
             'columnFields' => [
-                'bg_r' => [
-                    'label'     => &$GLOBALS['TL_LANG']['tl_content']['bg_r'],
+                'step_text' => [
+                    'label'     => &$GLOBALS['TL_LANG']['tl_content']['step_text'],
                     'exclude'   => true,
                     'inputType' => 'text',
                 ],
-                'bg_g' => [
-                    'label'     => &$GLOBALS['TL_LANG']['tl_content']['bg_b'],
+                'step_picture' => [
+                    'label'     => &$GLOBALS['TL_LANG']['tl_content']['step_picture'],
                     'exclude'   => true,
                     'inputType' => 'text',
                 ],
