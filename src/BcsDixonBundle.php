5614132e00019666879baf1d1f8b5160fb369d35
Bright Cloud Studio <mark@brightcloudstudio.com>
2024-12-17 10:43:18 -0500

    Create BcsDixonBundle.php

---
+++ src/BcsDixonBundle.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @copyright  Bright Cliud Studio
+ * @author     Bright Cloud Studio
+ * @package    Dixon Inspiration
+ * @license    LGPL-3.0+
+ * @see	       https://github.com/bright-cloud-studio/dixon-inspiration
+ */
+
+namespace Bcs\DixonBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class BcsDixonBundle extends Bundle
+{
+}
