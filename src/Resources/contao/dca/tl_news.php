61f4776b46faee5c553c6c46c58e332cdd9c4032
Bright Cloud Studio <mark@brightcloudstudio.com>
2024-12-17 12:02:32 -0500

    Update tl_news.php

--- src/Resources/contao/dca/tl_news.php
+++ src/Resources/contao/dca/tl_news.php
@@ -1,21 +1,26 @@
 <?php
 
  /* Extend the tl_news palettes */
-$GLOBALS['TL_DCA']['tl_news']['palettes']['default'] = str_replace(';{enclosure_legend:hide}', ';{reader_main_image_legend},addImageMainImage;{enclosure_legend:hide}', $GLOBALS['TL_DCA']['tl_news']['palettes']['default']);
+$GLOBALS['TL_DCA']['tl_news']['palettes']['default'] = str_replace(';{enclosure_legend:hide}', ';{type_legend},news_type;{enclosure_legend:hide}', $GLOBALS['TL_DCA']['tl_news']['palettes']['default']);
 
-$GLOBALS['TL_DCA']['tl_news']['palettes']['__selector__'] = array('source', 'addImageMainImage', 'addImage', 'addEnclosure', 'overwriteMeta');
-$GLOBALS['TL_DCA']['tl_news']['subpalettes']['addImageMainImage'] = 'singleSRCMainImage,sizeMainImage';
+$GLOBALS['TL_DCA']['tl_news']['palettes']['__selector__'] = array('source', 'addImage', 'addEnclosure', 'overwriteMeta', 'news_type');
+$GLOBALS['TL_DCA']['tl_news']['subpalettes']['step_by_step'] = 'singleSRCMainImage';
+$GLOBALS['TL_DCA']['tl_news']['subpalettes']['activity'] = 'sizeMainImage';
 
 
 
-
-$GLOBALS['TL_DCA']['tl_news']['fields']['addImageMainImage'] = array
+$GLOBALS['TL_DCA']['tl_news']['fields']['news_type'] = array
 (
-    'exclude'                 => true,
-    'inputType'               => 'checkbox',
-    'eval'                    => array('submitOnChange'=>true),
-    'sql'                     => "char(1) NOT NULL default ''"
+    'label'                    => &$GLOBALS['TL_LANG']['tl_news']['news_type'],
+    'inputType'                => 'select',
+    'options'                  => array('step_by_step' => 'Step-by-Step', 'activity' => 'Activity', 'article' => 'Article'),
+    'eval'                     => array('submitOnChange'=>true, 'mandatory'=>true, 'tl_class'=>'w50'),
+    'default'                   => '',
+    'sql'                       => "text default ''"
 );
+
+
+
 $GLOBALS['TL_DCA']['tl_news']['fields']['singleSRCMainImage'] = array
 (
     'label'                   => &$GLOBALS['TL_LANG']['tl_content']['singleSRCMainImage'],
