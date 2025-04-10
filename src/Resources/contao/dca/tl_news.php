8f42c0bbfa9c86266c7eb15a03b3d7402bbb45b9
johnadapt <2372574+johnadapt@users.noreply.github.com>
2025-04-10 11:14:37 -0700

    Update tl_news.php
    quick fixes to make it like the live site

--- src/Resources/contao/dca/tl_news.php
+++ src/Resources/contao/dca/tl_news.php
@@ -1,57 +1,65 @@
 <?php
 
+/**
+ * @copyright  Bright Cloud Studio
+ * @author     Bright Cloud Studio
+ * @package    Contao CE Chart
+ * @license    LGPL-3.0+
+ * @see        https://github.com/bright-cloud-studio/dixon-inspiration
+ */
+
+use Contao\DataContainer;
+use Contao\Database;
+use Contao\Input;
+use Contao\System;
+
 // Insert the newsType field into the palette at an appropriate spot.
-// (Make sure to use a legend marker that actually exists in tl_news's palette.)
-$GLOBALS['TL_DCA']['tl_news']['palettes']['default'] = str_replace('{title_legend}', '{type_legend},newsType;{title_legend}', $GLOBALS['TL_DCA']['tl_news']['palettes']['default']);
+$GLOBALS['TL_DCA']['tl_news']['palettes']['default'] = str_replace(
+    '{title_legend}',
+    '{type_legend},newsType;{title_legend}',
+    $GLOBALS['TL_DCA']['tl_news']['palettes']['default']
+);
+
 // Append 'newsType' to the existing __selector__ array rather than overwriting it.
 $GLOBALS['TL_DCA']['tl_news']['palettes']['__selector__'][] = 'newsType';
 
- /* Extend the tl_news palettes */
-$GLOBALS['TL_DCA']['tl_news']['palettes']['default'] = str_replace(';{teaser_legend}', ';{add_news_fields_legend},news_issue;{teaser_legend}', $GLOBALS['TL_DCA']['tl_news']['palettes']['default']);
-
-$GLOBALS['TL_DCA']['tl_news']['fields']['news_issue'] = array
-(
-    'label'                   => &$GLOBALS['TL_LANG']['tl_news']['news_issue'],
-    'inputType'               => 'text',
-    'default'                 => '',
-    'search'                  => true,
-    'eval'                    => array('mandatory'=>false, 'tl_class'=>'w50'),
-    'sql'                     => "varchar(255) NOT NULL default ''"
-);
-
-// Define subpalettes for the various newsType options. - removed singleSRCMainImage
-$GLOBALS['TL_DCA']['tl_news']['subpalettes']['newsType_step'] = 'stepImage, stepDownload, stepVideo, stepDixonMaterials, stepOtherMaterials, objectives';
+// Define subpalettes for the various newsType options
+$GLOBALS['TL_DCA']['tl_news']['subpalettes']['newsType_step'] = 'stepImage, stepDownload, stepVideo, stepDixonMaterials, stepOtherMaterials, stepObjectives';
 $GLOBALS['TL_DCA']['tl_news']['subpalettes']['newsType_activity'] = 'sizeMainImage';
-// Blank subpalette for "default" – no extra fields are added when "default" is selected.
 $GLOBALS['TL_DCA']['tl_news']['subpalettes']['newsType_default'] = '';
 
 // Fields for the 'newsType' selection
-$GLOBALS['TL_DCA']['tl_news']['fields']['newsType'] = array
-(
+$GLOBALS['TL_DCA']['tl_news']['fields']['newsType'] = array(
     'label'     => &$GLOBALS['TL_LANG']['tl_news']['newsType'],
     'inputType' => 'select',
-    'options'   => array('step' => 'Step-by-Step', 'activity' => 'Activity', 'default' => 'Default'),
+    'options'   => array(
+        'step' => 'Step-by-Step',
+        'activity' => 'Activity',
+        'default' => 'Default'
+    ),
     'eval'      => array('submitOnChange' => true, 'mandatory' => true, 'tl_class' => 'w50'),
     'default'   => 'default',
-    'sql'       => "text default 'default'"
+    'sql'       => "varchar(255) NOT NULL default ''"
 );
 
-$GLOBALS['TL_DCA']['tl_news']['fields']['sizeMainImage'] = array
-(
-    'label'            => &$GLOBALS['TL_LANG']['MSC']['sizeMainImage'],
-    'exclude'          => true,
-    'inputType'        => 'imageSize',
-    'reference'        => &$GLOBALS['TL_LANG']['MSC'],
-    'eval'             => array('rgxp' => 'natural', 'includeBlankOption' => true, 'nospace' => true, 'helpwizard' => true, 'tl_class' => 'w50'),
-    'options_callback' => static function ()
-    {
-        return System::getContainer()->get('contao.image.sizes')->getOptionsForUser(BackendUser::getInstance());
-    },
-    'sql'              => "varchar(64) NOT NULL default ''"
-);
+// Ensure newsType dropdown appears when editing old content
+$GLOBALS['TL_DCA']['tl_news']['config']['onload_callback'][] = function (DataContainer $dc) {
+    if (!$dc->id) {
+        return;
+    }
 
-$GLOBALS['TL_DCA']['tl_news']['fields']['stepImage'] = array
-(
+    $newsType = Database::getInstance()
+        ->prepare("SELECT newsType FROM tl_news WHERE id=?")
+        ->execute($dc->id)
+        ->newsType;
+
+    if (!empty($newsType)) {
+        Input::setGet('act', 'edit');
+    }
+};
+
+// Define fields for Step type
+$GLOBALS['TL_DCA']['tl_news']['fields']['stepImage'] = array(
     'label'     => &$GLOBALS['TL_LANG']['MSC']['stepImage'],
     'exclude'   => true,
     'inputType' => 'fileTree',
@@ -64,8 +72,7 @@ $GLOBALS['TL_DCA']['tl_news']['fields']['stepImage'] = array
     'sql'       => "binary(16) NULL"
 );
 
-$GLOBALS['TL_DCA']['tl_news']['fields']['stepDownload'] = array
-(
+$GLOBALS['TL_DCA']['tl_news']['fields']['stepDownload'] = array(
     'label'     => &$GLOBALS['TL_LANG']['MSC']['stepDownload'],
     'exclude'   => true,
     'inputType' => 'fileTree',
@@ -73,8 +80,7 @@ $GLOBALS['TL_DCA']['tl_news']['fields']['stepDownload'] = array
     'sql'       => "binary(16) NULL"
 );
 
-$GLOBALS['TL_DCA']['tl_news']['fields']['stepVideo'] = array
-(
+$GLOBALS['TL_DCA']['tl_news']['fields']['stepVideo'] = array(
     'label'     => &$GLOBALS['TL_LANG']['MSC']['stepVideo'],
     'exclude'   => true,
     'inputType' => 'textarea',
@@ -83,9 +89,7 @@ $GLOBALS['TL_DCA']['tl_news']['fields']['stepVideo'] = array
     'sql'       => "mediumtext NULL"
 );
 
-
-$GLOBALS['TL_DCA']['tl_news']['fields']['stepDixonMaterials'] = array
-(
+$GLOBALS['TL_DCA']['tl_news']['fields']['stepDixonMaterials'] = array(
     'label'     => &$GLOBALS['TL_LANG']['MSC']['stepDixonMaterials'],
     'exclude'   => true,
     'inputType' => 'textarea',
@@ -94,8 +98,7 @@ $GLOBALS['TL_DCA']['tl_news']['fields']['stepDixonMaterials'] = array
     'sql'       => "mediumtext NULL"
 );
 
-$GLOBALS['TL_DCA']['tl_news']['fields']['stepOtherMaterials'] = array
-(
+$GLOBALS['TL_DCA']['tl_news']['fields']['stepOtherMaterials'] = array(
     'label'     => &$GLOBALS['TL_LANG']['MSC']['stepOtherMaterials'],
     'exclude'   => true,
     'inputType' => 'textarea',
@@ -104,12 +107,31 @@ $GLOBALS['TL_DCA']['tl_news']['fields']['stepOtherMaterials'] = array
     'sql'       => "mediumtext NULL"
 );
 
-$GLOBALS['TL_DCA']['tl_news']['fields']['objectives'] = array
+$GLOBALS['TL_DCA']['tl_news']['fields']['stepObjectives'] = array
 (
-    'label'     => &$GLOBALS['TL_LANG']['MSC']['objectives'],
+    'label'     => &$GLOBALS['TL_LANG']['MSC']['stepObjectives'],
     'exclude'   => true,
     'inputType' => 'textarea',
     'search'    => true,
     'eval'      => array('style' => 'height:60px', 'rte' => 'tinyMCE', 'tl_class' => 'clr long'),
     'sql'       => "mediumtext NULL"
+);
+
+// Define fields for Activity type
+$GLOBALS['TL_DCA']['tl_news']['fields']['sizeMainImage'] = array(
+    'label'            => &$GLOBALS['TL_LANG']['MSC']['sizeMainImage'],
+    'exclude'          => true,
+    'inputType'        => 'imageSize',
+    'reference'        => &$GLOBALS['TL_LANG']['MSC'],
+    'eval'             => array(
+        'rgxp' => 'natural',
+        'includeBlankOption' => true,
+        'nospace' => true,
+        'helpwizard' => true,
+        'tl_class' => 'w50'
+    ),
+    'options_callback' => static function () {
+        return System::getContainer()->get('contao.image.sizes')->getOptionsForUser(BackendUser::getInstance());
+    },
+    'sql'              => "varchar(64) NOT NULL default ''"
 );
